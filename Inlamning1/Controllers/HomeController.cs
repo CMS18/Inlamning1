6cc20f60c25bda5d1d5993d031383a34f20edee9
Parang <43036653+ParangL@users.noreply.github.com>
2019-11-14 13:01:28 +0100

    Customer,Account,Repo classes

--- Inlamning1/Controllers/HomeController.cs
+++ Inlamning1/Controllers/HomeController.cs
@@ -5,39 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Inlamning1.Models;
+using Inlamning1.Models.ViewModels;
+using Inlamning1Data.Data;
 
 namespace Inlamning1.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
+{    
+        public class HomeController : Controller
         {
-            return View();
-        }
+            private readonly BankRepository repo;
+            private HomeViewModel model;
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            public HomeController(BankRepository repo)
+            {
+                this.repo = repo;
+                model = new HomeViewModel();
+            }
+            public IActionResult Index()
+            {
+                model.Customers = repo.GetAllCustomers().ToList();
+                return View(model);
+            }
 
-            return View();
-        }
+            public IActionResult Privacy()
+            {
+                return View();
+            }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
-}
